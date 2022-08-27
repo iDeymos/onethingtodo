5acedc0dad4cc559c95b439caeae25df396d9c2d
iDeymos <ninzak@ya.ru>
2022-08-27 23:09:03 +0500

    Key changes

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
-  const handleTextPress = () => console.log('Тапнули по тексту');
+  const handleButtonPress = () => Alert.alert("Onethingtodo сообщает", "Первое тестовое сообщение", [
+    {text: "Ok", onPress: () => console.log('Oke')},
+    {text: "Cancel", onPress: () => console.log('Niet')}
+  ]);
+  const handlePressButton2 = () => Alert.prompt("Запрос данных", "Введите ваши данные", text => console.log(text));
 
   return (
     <View style={styles.container}>
-      <Text numberOfLines={2} style={styles.text} onPress={handleTextPress}>Первое приложение {'\n'}тестовое сообщение</Text>
-      <Button title={'Старт'} color="red" onPress={handleTextPress}/>
+      <Text numberOfLines={2} style={styles.text}>Первое приложение {'\n'}тестовое сообщение</Text>
+      <Button title={'Старт'} color="red" onPress={handleButtonPress}/>
+      <Button title={'Введите данные'} onPress={handlePressButton2}/>
       <StatusBar style="auto" />
     </View>
   );
