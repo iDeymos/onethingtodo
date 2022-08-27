12371ccbffd5a7b107c632219ced00d097ea55c0
iDeymos <ninzak@ya.ru>
2022-08-27 22:01:34 +0500

    Обучение React Native

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const handleTextPress = () => console.log('Тапнули по тексту');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={2} style={styles.text} onPress={handleTextPress}>Первое приложение {'\n'}тестовое сообщение</Text>
+      <Button title={'Старт'} color="red" onPress={handleTextPress}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +18,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: 'red'
   },
 });
