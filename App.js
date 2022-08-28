1741e3258cfc5528ec22c8ecb22f551f98b9bb5d
iDeymos <ninzak@ya.ru>
2022-08-29 00:37:25 +0500

    flex-boxes

--- App.js
+++ App.js
@@ -1,31 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native';
 
 export default function App() {
   const handleButtonPress = () => Alert.alert("Onethingtodo сообщает", "Первое тестовое сообщение", [
     {text: "Ok", onPress: () => console.log('Oke')},
     {text: "Cancel", onPress: () => console.log('Niet')}
   ]);
-  const handlePressButton2 = () => Alert.prompt("Запрос данных", "Введите ваши данные", text => console.log(text));
+
+  const messageBox = <Text style={styles.box}>- Hello there!</Text>
+  const messageBox2 = <Text style={styles.box}>- General Kenobi.</Text>
 
   return (
-    <View style={styles.container}>
-      <Text numberOfLines={2} style={styles.text}>Первое приложение {'\n'}тестовое сообщение</Text>
-      <Button title={'Старт'} color="red" onPress={handleButtonPress}/>
-      <Button title={'Введите данные'} onPress={handlePressButton2}/>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.mainBlock}>
+        <View style={[styles.box, {backgroundColor: 'red'}]}>
+          <Text>To do {'\n'}</Text>
+        </View>
+        <View style={[styles.box, {backgroundColor: 'grey'}]}>
+          <Text>When {'\n'}Today</Text>
+        </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainBlock: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'baseline'
   },
-  text: {
-    color: 'red'
-  },
+  box: {
+    width: 100,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
