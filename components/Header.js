ce44aa9ffa30942c13aa17499b9d93e0590d9f40
iDeymos <ninzak@ya.ru>
2022-08-30 05:16:15 +0500

    Обновление списка дел

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ListItem() {
+  return (
+    <View style={styles.main}>
+        <Text style={styles.text}>Список дел</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    main: {
+        paddingTop: 50,
+        height: 100,
+        backgroundColor: 'silver'
+    },
+    text: {
+        fontSize: 18,
+        color: 'Red',
+        fontStyle: 'bold',
+        textAlign: 'center'
+    }
+
+});
